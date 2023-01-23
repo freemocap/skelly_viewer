5c868906ee7c244fb82bc7a8e864716d491a25e0
aaroncherian <aaron.cherian2@gmail.com>
2023-01-23 14:15:14 -0500

    setting up for skelly viewer

---
+++ skelly_viewer/config/folder_directory.py
@@ -0,0 +1,11 @@
+
+
+DATA_FOLDER_NAME = 'DataArrays'
+
+#DATA_FOLDER_NAME = 'output_data'
+
+MEDIAPIPE_3D_BODY_FILE_NAME = 'mediaPipeSkel_3d_origin_aligned.npy'
+
+#MEDIAPIPE_3D_BODY_FILE_NAME = 'mediapipe_body_3d_xyz.npy'
+
+TOTAL_BODY_CENTER_OF_MASS_NPY_FILE_NAME = "totalBodyCOM_frame_XYZ.npy"
---
+++ skelly_viewer/utils/freemocap_data_loader.py
@@ -0,0 +1,24 @@
+
+from config.folder_directory import DATA_FOLDER_NAME, MEDIAPIPE_3D_BODY_FILE_NAME,TOTAL_BODY_CENTER_OF_MASS_NPY_FILE_NAME
+
+from pathlib import Path
+
+import numpy as np
+
+class FreeMoCapDataLoader():
+    def __init__(self, path_to_session_folder:Path):
+        self.path_to_session_folder = path_to_session_folder
+
+    def load_mediapipe_body_data(self):
+        self.path_to_mediapipe_body_data = self.path_to_session_folder/DATA_FOLDER_NAME/MEDIAPIPE_3D_BODY_FILE_NAME
+        mediapipe_body_data = np.load(self.path_to_mediapipe_body_data)
+        return mediapipe_body_data
+
+    def load_total_body_COM_data(self):
+        self.path_to_total_body_COM_data = self.path_to_session_folder/DATA_FOLDER_NAME/TOTAL_BODY_CENTER_OF_MASS_NPY_FILE_NAME
+        total_body_COM_data = np.load(self.path_to_total_body_COM_data)
+        return total_body_COM_data
+
+
+
+
