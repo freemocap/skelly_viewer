0a3ccc7514a3a8e359130c145a64449d5b04ddb9
philipqueen <queen.philip@gmail.com>
2023-01-30 11:10:52 -0700

    Add working video import for macs/linux

--- skelly_viewer/utils/GUI_widgets/multi_camera_capture_widget.py
+++ skelly_viewer/utils/GUI_widgets/multi_camera_capture_widget.py
@@ -46,9 +46,17 @@ class MultiVideoDisplay(QWidget):
 
     def create_list_of_video_paths(self, path_to_video_folder: Path):
         # search the folder for 'mp4' files and create a list of them
-        list_of_video_paths = list(Path(path_to_video_folder).glob('*.mp4'))
-        number_of_videos = len(list_of_video_paths)
-        return list_of_video_paths, number_of_videos
+        list_of_video_paths = list(Path(path_to_video_folder).glob('*.mp4')) + list(Path(path_to_video_folder).glob('*.MP4'))
+        unique_list_of_video_paths = self.get_unique_list(list_of_video_paths)
+        number_of_videos = len(unique_list_of_video_paths)
+        return unique_list_of_video_paths, number_of_videos
+
+    def get_unique_list(self, list: list) -> list:
+        '''Return a list of the unique elements from input list'''
+        unique_list = []
+        [unique_list.append(clip) for clip in list if clip not in unique_list]
+
+        return unique_list
 
     def generate_video_display(self, list_of_video_paths: list, number_of_videos: int):
         self.video_worker_dictionary = self.generate_video_workers(list_of_video_paths)
