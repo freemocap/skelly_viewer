2dbd26cc0a50b58079199fad6b1b0b547b70fc3a
jonmatthis <jonmatthis@gmail.com>
2023-05-25 14:23:11 -0400

    Bump version v2023.05.1018 -> v2023.05.1019

--- skelly_viewer/__init__.py
+++ skelly_viewer/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for basic_template_repo."""
 
 __package_name__ = "skelly_viewer"
-__version__ = "v2023.05.1018"
+__version__ = "v2023.05.1019"
 
 __author__ = """Skelly FreeMoCap"""
 __email__ = "info@freemocap.org"
