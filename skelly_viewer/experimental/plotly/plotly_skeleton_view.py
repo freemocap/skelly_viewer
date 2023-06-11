e92d6bdea7f3384ac715fe58142a56d16cb0ac64
jonmatthis <jonmatthis@gmail.com>
2023-06-11 12:27:40 -0400

    break up classes

--- skelly_viewer/experimental/plotly/plotly_skeleton_view.py
+++ skelly_viewer/experimental/plotly/plotly_skeleton_view.py
@@ -8,18 +8,9 @@ from dash import Dash
 from dash import dcc, html
 
 
-class SkeletonViewer:
+class DataLoader:
     def __init__(self, recording_folder_path: Union[str, Path]):
         self.recording_folder_path = Path(recording_folder_path)
-        self.data_by_frame = self.load_data_by_frame()
-        self.data_by_trajectory = self.load_data_by_trajectory()
-        self.ax_range = 5000
-        self.axis_props = dict(
-            showbackground=True,
-            backgroundcolor="rgb(230, 230,230)",
-            gridcolor="rgb(255, 255, 255)",
-            zerolinecolor="rgb(255, 255, 255)",
-        )
 
     def load_data_by_trajectory(self):
         file_name = f"{self.recording_folder_path.name}_by_trajectory.csv"
@@ -34,33 +25,43 @@ class SkeletonViewer:
             data = json.load(f)
         return data["data_by_frame"]
 
-    def create_frames(self):
-        frames = []
-        for frame_number, frame_data in self.data_by_frame.items():
-            frames.append(self.create_frame(frame_number, frame_data))
-        return frames
 
-    def create_frame(self, frame_number, frame_data):
-        center_of_mass = frame_data["center_of_mass"]["full_body_com"]
+class FrameCreator:
+    @staticmethod
+    def create_frame(frame_number, frame_data):
         body = frame_data["body"]
         body_names = list(body.keys())
-        return go.Frame(data=[
-            go.Scatter3d(
-                x=[point["x"] for point in body.values()],
-                y=[point["y"] for point in body.values()],
-                z=[point["z"] for point in body.values()],
-                mode='markers',
-                marker=dict(
-                    size=3,
-                    color='purple',
-                    colorscale='Viridis',  # choose a colorscale
-                ),
-                ids=body_names,
-
-            )
-        ],
+        return go.Frame(
+            data=[
+                go.Scatter3d(
+                    x=[point["x"] for point in body.values()],
+                    y=[point["y"] for point in body.values()],
+                    z=[point["z"] for point in body.values()],
+                    mode='markers',
+                    marker=dict(
+                        size=3,
+                        color='purple',
+                        colorscale='Viridis',  # choose a colorscale
+                    ),
+                    ids=body_names,
+                )
+            ],
             name=str(frame_number)
         )
+
+    @staticmethod
+    def create_frames(data_by_frame):
+        frames = []
+        for frame_number, frame_data in data_by_frame.items():
+            frames.append(FrameCreator.create_frame(frame_number, frame_data))
+        return frames
+
+
+class FigureCreator:
+    def __init__(self, axis_props, ax_range, updatemenus):
+        self.axis_props = axis_props
+        self.ax_range = ax_range
+        self.updatemenus = updatemenus
 
     def create_figure(self, frames):
         fig = go.Figure(
@@ -72,29 +73,76 @@ class SkeletonViewer:
                     zaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
                     aspectmode='cube'
                 ),
-                updatemenus=[dict(
-                    type='buttons',
-                    showactive=False,
-                    buttons=[dict(
-                        label='Play',
-                        method='animate',
-                        args=[None, {"frame": {"duration": 30}}]
-                    )]
-                )]
+                updatemenus=self.updatemenus
             ),
             frames=frames
         )
 
         return fig
 
-    def create_dash_app(self):
-        frames = self.create_frames()
-        fig = self.create_figure(frames)
 
+class DashAppCreator:
+    @staticmethod
+    def create_dash_app(figure):
         app = Dash(__name__)
         app.layout = html.Div([
-            dcc.Graph(figure=fig)
-        ])
+            dcc.Graph(figure=figure, style={'height': '800px', 'width': '100%'})
+        ], style={'height': '800px', 'width': '100%'})
+
+        return app
+
+
+class WidgetManager:
+    @staticmethod
+    def add_button(layout, label, method, args):
+        layout.append(dict(
+            type='buttons',
+            showactive=False,
+            buttons=[dict(
+                label=label,
+                method=method,
+                args=args
+            )]
+        ))
+
+    @staticmethod
+    def add_slider(layout, min_value, max_value, value, step):
+        layout.append(dcc.Slider(
+            min=min_value,
+            max=max_value,
+            value=value,
+            step=step
+        ))
+
+    @staticmethod
+    def add_widgets(layout):
+        WidgetManager.add_button(layout, 'Play', 'animate', [None, {"frame": {"duration": 30}}])
+        # You can add more buttons, sliders, etc here.
+
+
+class SkeletonViewer:
+    def __init__(self, recording_folder_path: Union[str, Path]):
+        self.recording_folder_path = Path(recording_folder_path)
+        self.ax_range = 5000
+        self.axis_props = dict(
+            showbackground=True,
+            backgroundcolor="rgb(230, 230,230)",
+            gridcolor="rgb(255, 255, 255)",
+            zerolinecolor="rgb(255, 255, 255)",
+        )
+        self.data_loader = DataLoader(self.recording_folder_path)
+
+    def create_app(self):
+        data_by_frame = self.data_loader.load_data_by_frame()
+        frames = FrameCreator.create_frames(data_by_frame)
+
+        updatemenus = []
+        WidgetManager.add_widgets(updatemenus)
+
+        figure_creator = FigureCreator(self.axis_props, self.ax_range, updatemenus)
+        fig = figure_creator.create_figure(frames)
+
+        app = DashAppCreator.create_dash_app(fig)
 
         return app
 
@@ -102,5 +150,5 @@ class SkeletonViewer:
 if __name__ == "__main__":
     recording_folder_path = r"C:\Users\jonma\freemocap_data\recording_sessions\freemocap_sample_data"
     viewer = SkeletonViewer(recording_folder_path)
-    app = viewer.create_dash_app()
+    app = viewer.create_app()
     app.run_server(debug=True)
