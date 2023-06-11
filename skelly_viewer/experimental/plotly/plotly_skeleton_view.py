2e8b4d89150966eaaf2949115ee57e7893eda4c6
jonmatthis <jonmatthis@gmail.com>
2023-06-11 12:01:03 -0400

    w dash

--- skelly_viewer/experimental/plotly/plotly_skeleton_view.py
+++ skelly_viewer/experimental/plotly/plotly_skeleton_view.py
@@ -1,10 +1,12 @@
 import json
 from pathlib import Path
-from typing import Union, Dict
-import numpy as np
+from typing import Union
+
 import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+from dash import Dash
+from dash import dcc, html
+
 
 class SkeletonViewer:
     def __init__(self, recording_folder_path: Union[str, Path]):
@@ -35,67 +37,70 @@ class SkeletonViewer:
     def create_frames(self):
         frames = []
         for frame_number, frame_data in self.data_by_frame.items():
-
             frames.append(self.create_frame(frame_number, frame_data))
         return frames
 
     def create_frame(self, frame_number, frame_data):
-
         center_of_mass = frame_data["center_of_mass"]["full_body_com"]
         body = frame_data["body"]
         body_names = list(body.keys())
         return go.Frame(data=[
-                go.Scatter3d(
-                    x=[point["x"] for point in body.values()],
-                    y=[point["y"] for point in body.values()],
-                    z=[point["z"] for point in body.values()],
-                    mode='markers',
-                    marker=dict(
-                        size=3,
-                        color='purple',
-                        colorscale='Viridis',  # choose a colorscale
-                    ),
-                    ids=body_names,
+            go.Scatter3d(
+                x=[point["x"] for point in body.values()],
+                y=[point["y"] for point in body.values()],
+                z=[point["z"] for point in body.values()],
+                mode='markers',
+                marker=dict(
+                    size=3,
+                    color='purple',
+                    colorscale='Viridis',  # choose a colorscale
+                ),
+                ids=body_names,
 
-                )
-            ],
+            )
+        ],
             name=str(frame_number)
         )
 
     def create_figure(self, frames):
-        # Create a figure with subplots
-        fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'scatter3d'}, {}]])
-
-        fig.add_trace(frames[0]['data'][0], row=1, col=1)
-
-        fig.update_layout(
-            scene=dict(
-                xaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
-                yaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
-                zaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
-                aspectmode='cube'
-            ),
-            updatemenus=[dict(
-                type='buttons',
-                showactive=False,
-                buttons=[dict(
-                    label='Play',
-                    method='animate',
-                    args=[None, {"frame": {"duration": 30}}]
+        fig = go.Figure(
+            data=[frames[0]['data'][0]],
+            layout=go.Layout(
+                scene=dict(
+                    xaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
+                    yaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
+                    zaxis=dict(self.axis_props, range=[-self.ax_range, self.ax_range]),
+                    aspectmode='cube'
+                ),
+                updatemenus=[dict(
+                    type='buttons',
+                    showactive=False,
+                    buttons=[dict(
+                        label='Play',
+                        method='animate',
+                        args=[None, {"frame": {"duration": 30}}]
+                    )]
                 )]
-            )]
+            ),
+            frames=frames
         )
 
-        fig.frames = frames
         return fig
 
-    def plotly_skeleton_view(self):
+    def create_dash_app(self):
         frames = self.create_frames()
         fig = self.create_figure(frames)
-        fig.show()
+
+        app = Dash(__name__)
+        app.layout = html.Div([
+            dcc.Graph(figure=fig)
+        ])
+
+        return app
 
 
 if __name__ == "__main__":
     recording_folder_path = r"C:\Users\jonma\freemocap_data\recording_sessions\freemocap_sample_data"
     viewer = SkeletonViewer(recording_folder_path)
-    viewer.plotly_skeleton_view()
+    app = viewer.create_dash_app()
+    app.run_server(debug=True)
