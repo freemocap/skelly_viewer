036ab4d8c45d36380844f1f76c1c8f7104692141
jonmatthis <jonmatthis@gmail.com>
2023-06-13 10:02:17 -0400

    saves vid

--- skelly_viewer/matplotlib/animation_creator.py
+++ skelly_viewer/matplotlib/animation_creator.py
@@ -1,8 +1,13 @@
+import logging
+from pathlib import Path
 from typing import Union
 
+import cv2
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import gridspec
+from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 from skelly_viewer.data_wrangling.data_loader import DataLoader
 from skelly_viewer.matplotlib.subplots.subplot_2d import Subplot2d
@@ -10,11 +15,19 @@ from skelly_viewer.matplotlib.subplots.subplot_3d import Subplot3d
 from skelly_viewer.matplotlib.subplots.timeseries_subplot import TimeseriesSubplot
 from skelly_viewer.matplotlib.subplots.video_subplot import VideoSubplot
 
+
+mpl_logger = logging.getLogger('matplotlib')
+mpl_logger.setLevel(logging.WARNING)
+
 FRAME_INTERVAL = 1000 / 30
 
 
 class AnimationCreator:
-    def __init__(self, data_loader: DataLoader):
+    def __init__(self,
+                 recording_path: Union[str, Path], ):
+        data_loader = DataLoader(recording_path)
+        self.recording_name = Path(recording_path).name
+        self.output_file = Path(recording_path) / f"{self.recording_name}_animation.mp4"
         self.fig = plt.figure()
         grid_spec = gridspec.GridSpec(2, 2,
                                       figure=self.fig,
@@ -51,6 +64,14 @@ class AnimationCreator:
         self.animation_running = True
         self.animation.event_source.stop()  # Initially stop the animation
 
+        # # Initialize OpenCV VideoWriter
+        # self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # or use another codec
+        # self.video_writer = cv2.VideoWriter(str(Path(recording_path) / f"{self.recording_name}.mp4"),
+        #                                     self.fourcc,
+        #                                     30.0,
+        #                                     (1000, 1000))
+
+
     def toggle_animation(self):
         if self.animation_running:
             self.animation.event_source.stop()
@@ -59,18 +80,28 @@ class AnimationCreator:
             self.animation.event_source.start()
             self.animation_running = True
     def animate(self, frame_number: Union[str, int]):
+        self.fig.suptitle(f"{self.recording_name} - Frame {frame_number}")
         self.subplot_3d.animate(frame_number)
         self.subplot_2d.animate(frame_number)
         self.subplot_timeseries.animate(frame_number)
         self.subplot_video.animate(frame_number)
 
+
+
     def show(self):
         self.animation.event_source.start()
         plt.show()
 
+    def save(self):
+        self.animation.save(self.output_file, writer='ffmpeg', fps=30)
+
 
 if __name__ == "__main__":
     SAMPLE_DATA_PATH = r"C:\Users\jonma\freemocap_data\recording_sessions\freemocap_sample_data"
-    data_loader = DataLoader(SAMPLE_DATA_PATH)
-    animator = AnimationCreator(data_loader)
+
+    animator = AnimationCreator(recording_path = SAMPLE_DATA_PATH)
     animator.show()
+
+    print("Saving animation...")
+    animator.save()
+    print("Animation saved!")
--- skelly_viewer/matplotlib/matplotlib_widget.py
+++ skelly_viewer/matplotlib/matplotlib_widget.py
@@ -1,6 +1,9 @@
 import sys
+from pathlib import Path
+from typing import Union
 
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QSlider
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 
 from skelly_viewer.data_wrangling.data_loader import DataLoader
@@ -8,37 +11,46 @@ from skelly_viewer.matplotlib.animation_creator import AnimationCreator
 
 
 class MatplotlibAnimation(QWidget):
-    def __init__(self, data_loader: DataLoader,  parent=None):
+    def __init__(self, recording_path: Union[str, Path], parent=None):
         super(MatplotlibAnimation, self).__init__(parent)
-
-        # Set up layout
-        self.layout = QVBoxLayout(self)
-
-        # Create Play/Pause button
-        self.playButton = QPushButton('Play/Pause')
-
-        self.playButton.clicked.connect(self.toggle_animation)
-
-        self.layout.addWidget(self.playButton)
 
         # Create animation
         self.data_loader = DataLoader(SAMPLE_DATA_PATH)
-        self.animation_creator = AnimationCreator(self.data_loader)
+        self.animation_creator = AnimationCreator(recording_path=recording_path)
+
+        # Set up layout
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
+
+        # Create Play/Pause button
+        self.playButton = QPushButton('Play/Pause')
+        self.playButton.clicked.connect(self.toggle_animation)
+        self.layout.addWidget(self.playButton)
+
+
+        # # Create frame slider
+        # self.frameSlider = QSlider(Qt.Orientation.Horizontal)
+        # self.frameSlider.setRange(0, self.animation_creator.number_of_frames - 1)  # Frame numbers as range
+        # self.frameSlider.valueChanged.connect(self.update_frame)
+        # self.layout.addWidget(self.frameSlider)
+
 
         # Add animation to layout
         self.canvas = FigureCanvasQTAgg(self.animation_creator.fig)
         self.layout.addWidget(self.canvas)
 
-        self.setLayout(self.layout)
-
     def toggle_animation(self):
         self.animation_creator.toggle_animation()
+
+    def update_frame(self, value):
+        self.animation_creator.animate(value)
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     SAMPLE_DATA_PATH = r"C:\Users\jonma\freemocap_data\recording_sessions\freemocap_sample_data"
-    data_loader = DataLoader(SAMPLE_DATA_PATH)
-    animation_widget = MatplotlibAnimation(data_loader=data_loader)
+
+    animation_widget = MatplotlibAnimation(recording_path=SAMPLE_DATA_PATH)
     animation_widget.show()
     sys.exit(app.exec())
