c7556670d0efbcad04b10397e083b1229b78e034
Jonathan Samir Matthis <jonmatthis@gmail.com>
2023-07-30 09:22:39 -0400

    update ptqtgraph

--- skelly_viewer/experimental/pyqtgraph/pyqtgl_viewer.py
+++ skelly_viewer/experimental/pyqtgraph/pyqtgl_viewer.py
@@ -1,46 +1,64 @@
+import math
+import time
+
 import numpy as np
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
 from pyqtgraph.Qt import QtCore
-import math
-import time
 
-app = pg.mkQApp("GLScatterPlotItem Example")
-w = gl.GLViewWidget()
-w.show()
-w.setWindowTitle('pyqtgraph example: Connected Spheres')
-w.setCameraPosition(distance=50)
 
-g = gl.GLGridItem()
-w.addItem(g)
+class SpherePathGenerator:
+    def __init__(self, number_of_spheres: int = 24):
+        self.number_of_spheres = number_of_spheres
 
-def generate_path(t):
-    positions = np.zeros((24, 3))
-    for i in range(24):
-        angle = 2 * math.pi * i / 24 + t
-        positions[i, 0] = 10 * math.sin(angle)
-        positions[i, 1] = 10 * math.cos(angle)
-        positions[i, 2] = math.sin(2 * angle)
-    return positions
+    def generate_path(self, time_factor: float):
+        positions = np.zeros((self.number_of_spheres, 3))
+        for i in range(self.number_of_spheres):
+            angle = 2 * math.pi * i / self.number_of_spheres + time_factor
+            positions[i, 0] = 10 * math.sin(angle)
+            positions[i, 1] = 10 * math.cos(angle)
+            positions[i, 2] = math.sin(2 * angle)
+        return positions
 
-pos = generate_path(0)
-size = np.full(24, 1)
-color = np.array([[1.0, 0.0, 0.0, 0.5]] * 24)
 
-sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
-w.addItem(sp1)
+class SpheresViewer:
+    def __init__(self, sphere_path_generator: SpherePathGenerator):
+        self.app = pg.mkQApp("GLScatterPlotItem Example")
+        self.view_widget = gl.GLViewWidget()
+        self.view_widget.setWindowTitle('pyqtgraph example: Connected Spheres')
+        self.view_widget.setCameraPosition(distance=50)
 
-start_time = time.time()
+        self.grid = gl.GLGridItem()
+        self.view_widget.addItem(self.grid)
 
-def update():
-    global sp1, start_time
-    t = (time.time() - start_time) * 0.1
-    pos = generate_path(t)
-    sp1.setData(pos=pos)
+        self.path_generator = sphere_path_generator
 
-t = QtCore.QTimer()
-t.timeout.connect(update)
-t.start(50)
+        initial_pos = self.path_generator.generate_path(0)
+        size = np.full(24, 1)
+        color = np.array([[1.0, 0.0, 0.0, 0.5]] * 24)
+
+        self.spheres_item = gl.GLScatterPlotItem(
+            pos=initial_pos, size=size, color=color, pxMode=False
+        )
+
+        self.view_widget.addItem(self.spheres_item)
+
+        self.start_time = time.time()
+
+    def update(self):
+        time_factor = (time.time() - self.start_time) * 0.1
+        new_positions = self.path_generator.generate_path(time_factor)
+        self.spheres_item.setData(pos=new_positions)
+
+    def start(self):
+        self.view_widget.show()
+        update_timer = QtCore.QTimer()
+        update_timer.timeout.connect(self.update)
+        update_timer.start(50)
+        pg.exec()
+
 
 if __name__ == '__main__':
-    pg.exec()
+    generator = SpherePathGenerator()
+    viewer = SpheresViewer(generator)
+    viewer.start()
